f79db24fd40672b03ca31f9fc30cbe3da7172a35
滝猫 <systakineko.tfs@gmail.com>
2023-07-08 19:53:50 +0700

    Update file list

--- Install.bat
+++ Install.bat
@@ -1,17 +1,16 @@
 @echo off
-%~d0/appx-installs
-cd "%~dp0/appx-installs"
+cd /d "%~dp0/appx-installs"
 echo Combining VHDx from scratch...
-../7zip-core/7za.exe x product.vhdx.001
-../7zip-core/7za.exe x system.vhdx.001
-../7zip-core/7za.exe x system_ext.vhdx.001
-../7zip-core/7za.exe x vendor.vhdx.001
+"../7zip-core/7za.exe" x product.vhdx.001
+"../7zip-core/7za.exe" x system.vhdx.001
+"../7zip-core/7za.exe" x system_ext.vhdx.001
+"../7zip-core/7za.exe" x vendor.vhdx.001
 if not exist Install.ps1 (
     echo "Install.ps1" is not found.
     echo Press any key to exit
     pause>nul
     exit 1
 ) else (
-    start powershell.exe -ExecutionPolicy Bypass -File .\Install.ps1
+    powershell.exe -ExecutionPolicy Bypass -File .\Install.ps1
     exit
 )
--- Restore-WSA.bat
+++ Restore-WSA.bat
@@ -1,11 +1,10 @@
 @echo off
-%~d0/appx-installs
-cd "%~dp0/appx-installs"
+cd /d "%~dp0/appx-installs"
 echo Combining VHDx from scratch...
-../7zip-core/7za.exe x product.vhdx.001
-../7zip-core/7za.exe x system.vhdx.001
-../7zip-core/7za.exe x system_ext.vhdx.001
-../7zip-core/7za.exe x vendor.vhdx.001
+"../7zip-core/7za.exe" x product.vhdx.001
+"../7zip-core/7za.exe" x system.vhdx.001
+"../7zip-core/7za.exe" x system_ext.vhdx.001
+"../7zip-core/7za.exe" x vendor.vhdx.001
 if not exist Install.ps1 (
     echo "Install.ps1" is not found.
     echo Press any key to exit
